2e57e431350e3ca7cceb5f36c5a6c4e204a20806
Иван Сивохин <nigganwd63@mail.ru>
2022-05-21 17:59:10 +0300

    java3

---
+++ src/main.java
@@ -0,0 +1,21 @@
+public class main {
+    public static void main(String[] args) {
+        int deposit = 1100;
+        int bonus;
+        int clientacc = 100;
+        int futclientacc;
+
+        if(deposit>1000) {
+            bonus = deposit/100;
+            futclientacc = clientacc + deposit + bonus;
+            System.out.println("Клиентский счет после пополнения: " + futclientacc);
+            System.out.println("Бонус: " + bonus);
+        }
+        else {
+            futclientacc = clientacc + deposit;
+            bonus = 0;
+            System.out.println("Клиентский счет после пополнения: " + futclientacc);
+            System.out.println("Бонус: " + bonus);
+        }
+    }
+}
